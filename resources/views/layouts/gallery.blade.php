6d748fb9d03c4a250e83eebb1a67de936a40ad0f
alvindo <alvindo56@gmail.com>
2024-03-30 16:57:23 +0700

    fix import local css

--- resources/views/layouts/gallery.blade.php
+++ resources/views/layouts/gallery.blade.php
@@ -10,7 +10,42 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 
     {{-- my style css --}}
-    <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+        .form-login .form-floating:focus-within,
+        .form-register .form-floating:focus-within {
+            z-index: 2;
+        }
+
+        .form-login input[type="text"] {
+            margin-bottom: -1px;
+            border-bottom-right-radius: 0;
+            border-bottom-left-radius: 0;
+        }
+
+        .form-login input[type="password"] {
+            border-top-left-radius: 0;
+            border-top-right-radius: 0;
+        }
+
+        .form-register input {
+            margin-bottom: -1px;
+            border-radius: 0px;
+            -webkit-border-radius: 0px;
+            -moz-border-radius: 0px;
+            -ms-border-radius: 0px;
+            -o-border-radius: 0px;
+        }
+
+        body {
+            background: #f4f4f4;
+        }
+
+        .banner {
+            background: #a770ef;
+            background: -webkit-linear-gradient(to right, #a770ef, #cf8bf3, #fdb99b);
+            background: linear-gradient(to right, #a770ef, #cf8bf3, #fdb99b);
+        }
+    </style>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
