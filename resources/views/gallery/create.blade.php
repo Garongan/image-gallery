f620f62042a2e8ae6bd33f38595be24e9b05dc63
alvindo <alvindo56@gmail.com>
2024-03-31 11:01:41 +0700

    fix setup deployment

--- resources/views/gallery/create.blade.php
+++ resources/views/gallery/create.blade.php
@@ -48,5 +48,5 @@
     </form>
     {{-- input end --}}
 </section>
-<script src="{{ mix('assets/js/script.js') }}"></script>
+<script src="{{ secure_asset('assets/js/script.js') }}"></script>
 @endsection
--- resources/views/layouts/gallery.blade.php
+++ resources/views/layouts/gallery.blade.php
@@ -7,10 +7,10 @@
     <title>Gallery - {{ $title }}</title>
 
     {{-- bootstrap css --}}
-    <link rel="stylesheet" href="{{ mix('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap.min.css') }}">
 
     {{-- my style css --}}
-    <link rel="stylesheet" href="{{ mix('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -108,7 +108,7 @@
     </div>
 
     {{-- bootstrap js --}}
-    <script src="{{ mix('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ secure_asset('assets/js/bootstrap.min.js') }}"></script>
 </body>
 
 </html>
